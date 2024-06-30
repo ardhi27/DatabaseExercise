47939a5f21a04cd7f5b7390b2e9215f7c1da3486
kmshyn76 <147911990+Ardhi27@users.noreply.github.com>
2024-06-30 17:00:57 +0800

    Add new solution

---
+++ SOLUTION_2.sql
@@ -0,0 +1,9 @@
+-- USE classicmodels
+
+-- SELECT customerName, city, country FROM customers WHERE country = "USA"
+
+-- SELECT DISTINCT productVendor FROM products
+
+-- SELECT customerNumber, checkNumber, paymentDate, amount FROM payments WHERE customerNumber = 124 ORDER BY paymentDate DESC
+
+-- SELECT productName, buyPrice, quantityInStock FROM products WHERE quantityInStock >= 1000 AND quantityInStock <= 3000 ORDER BY buyPrice ASC LIMIT 5, 10
